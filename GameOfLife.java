5b76bfda1b2abc29ea82ea7dda9a749f1d8919b7
Alon Cory <alon.cory@runi.ac.il>
2024-01-15 18:56:45 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -129,10 +129,10 @@ public class GameOfLife {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
 		for (int i = 1; i < arr.length - 1; i++) {
+			System.out.println(); // Skips to the next line
 			for (int j = 1; j < arr[0].length - 1; j++) {
 				System.out.printf("%3s ", arr[i][j]);
 			}
-			System.out.println(); // Skips to the next line
 			System.out.println(); // Skips to the next line
 		}
 	}
